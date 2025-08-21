5334f5cb8d6abf1463b026a09347141bdf14b920
AlexRimmerHMRC <31283208+AlexRimmerHMRC@users.noreply.github.com>
2025-08-21 11:29:31 +0100

    Updated dependencies to resolve vulnerability

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.12.0"
+  private val bootstrapVersion = "10.1.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"   %% "bootstrap-backend-play-30" % bootstrapVersion,
-    "org.typelevel" %% "cats-core"                 % "2.9.0",
+    "org.typelevel" %% "cats-core"                 % "2.13.0",
   )
 
   val test: Seq[ModuleID] = Seq(
